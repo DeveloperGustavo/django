031ba75525d33fdf00bfce596406cc997aee0751
Gustavo Tavares Barbosa <gustavo.barbosa@rodobens.com.br>
2020-07-13 09:15:28 -0300

    configurando banco de dados

--- gerenciador_tarefas/app/models.py
+++ gerenciador_tarefas/app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Tarefa(models.Model):
+    PRIODADE_CHOICES = [
+        ("A", "Alta"),
+        ("B", "Baixa"),
+        ("N", "Normal")
+    ]
+    titulo = models.CharField(max_length=30, null=False, blank=False)
+    descricao = models.CharField(max_length=100, null=False, blank=False)
+    data_expiracao = models.DateField(null=False, blank=False)
+    prioridade = models.CharField(max_length=1, choices=PRIODADE_CHOICES, null=False, blank=False)
--- gerenciador_tarefas/app/views.py
+++ gerenciador_tarefas/app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import TarefaForms
 
 # Create your views here.
 
@@ -6,5 +7,6 @@ def listar_tarefas(request):
     nome_tarefa = 'Minha segunda condição em python'
     return render(request, 'tarefas/listar_tarefas.html', {'nome_tarefa': nome_tarefa})
 
-
-
+def cadastrar_tarefa(request):
+    form_tarefa = TarefaForms()
+    return render(request, 'tarefas/form_tarefas.html')
